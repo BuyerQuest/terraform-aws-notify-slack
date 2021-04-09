5197b7bd7d7e89cf09df5f1a24275052c70c5780
Maxim Putilin <maxim.putilin@buyerquest.com>
2021-04-10 00:07:49 +0700

    DEVOPS-3508: Parse whole event json, not just sns.message

--- functions/snsToSlack.js
+++ functions/snsToSlack.js
@@ -2,38 +2,37 @@ var https = require('https');
 var util = require('util');
 
 exports.handler = function (event, context) {
+  //console.log('[DEBUG] Received event:', JSON.stringify(event, null, 2));
 
-    var message_object = JSON.parse(event.Records[0].Sns.Message);
+  var slack_text = ""
+  eval("slack_text=" + process.env.SLACK_MESSAGE);
 
-    var slack_text = ""
-    eval("slack_text="+process.env.SLACK_MESSAGE);
+  var postData = {
+    "channel": process.env.SLACK_CHANNEL,
+    "username": process.env.SLACK_USERNAME,
+    "text": slack_text,
+    "icon_emoji": process.env.SLACK_EMOJI,
+    "mrkdwn": process.env.SLACK_MARKDOWN_ENABLE || "true",
+  };
 
-    var postData = {
-        "channel": process.env.SLACK_CHANNEL,
-        "username": process.env.SLACK_USERNAME,
-        "text": slack_text,
-        "icon_emoji": process.env.SLACK_EMOJI,
-        "mrkdwn": process.env.SLACK_MARKDOWN_ENABLE || "true",
-    };
+  var options = {
+    method: 'POST',
+    hostname: process.env.SLACK_HOSTNAME || "hooks.slack.com",
+    port: process.env.SLACK_PORT || "443",
+    path: process.env.SLACK_WEBHOOK_PATH
+  };
 
-    var options = {
-        method: 'POST',
-        hostname: process.env.SLACK_HOSTNAME || "hooks.slack.com",
-        port: process.env.SLACK_PORT || "443",
-        path: process.env.SLACK_WEBHOOK_PATH
-    };
-
-    var req = https.request(options, function(res) {
-      res.setEncoding('utf8');
-      res.on('data', function (chunk) {
-        context.done(null);
-      });
+  var req = https.request(options, function (res) {
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      context.done(null);
     });
+  });
 
-    req.on('error', function(e) {
-      console.log('[ERROR] Problem with request: ' + e.message);
-    });
+  req.on('error', function (e) {
+    console.log('[ERROR] Problem with request: ' + e.message);
+  });
 
-    req.write(util.format("%j", postData));
-    req.end();
+  req.write(util.format("%j", postData));
+  req.end();
 };
